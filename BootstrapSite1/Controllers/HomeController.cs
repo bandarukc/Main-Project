61609f4bc10446851573cd1a3832df95a1b22ab4
bandarukc <bandarukc@gmail.com>
2016-02-11 23:00:14 +0530

    initial commit

---
+++ BootstrapSite1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapSite1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title="Home";
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            
+            return View();
+        }
+        public ActionResult Posts()
+        {
+            
+            return View();
+        }
+        public ActionResult History()
+        {
+            
+            return View();
+        }
+        
+
+    }
+}
